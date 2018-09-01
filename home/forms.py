ea477b41c9f3dd9f11acf9e973c7ce8210f742da
fhall21 <felix.p.hall@gmail.com>
2018-09-01 16:58:12 +1000

    fixed sintax error

--- home/forms.py
+++ home/forms.py
@@ -2,12 +2,12 @@ from django import forms
 
 class TemplateForm(forms.Form):
     message = forms.CharField(label='Message:', 
-    	max_length=500
+    	max_length=500,
     	help_text='How you do #DiversityFIRST within your team.',
     	min_length=0,
     	required=True)
     team_name = forms.CharField(label='Team Name:', 
     	max_length=100,
     	min_length=0,
-    	helpt_text='Your team name.'
+    	help_text='Your team name.',
     	required=True)
