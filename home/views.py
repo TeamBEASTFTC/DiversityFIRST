b68e8cd1cab47da73219842729e50d1f7350c854
fhall21 <felix.p.hall@gmail.com>
2018-09-08 18:18:22 +1000

    added it all to the front page - the template creator. And made preview not get too big. And made another download button appear when the brakpoint is pasted

--- home/views.py
+++ home/views.py
@@ -57,9 +57,44 @@ class TemplatePage(TemplateView):
 class HomePage(TemplateView):
 	template_name = 'home/home_page.html'
 
-	def get(self, request):
-		return render(request, self.template_name)
 
+	def get(self, request):
+		template_form = TemplateForm()
+		args = {'form': template_form}
+		return render(request, self.template_name, args)
+
+	def post(self, request):
+		form = TemplateForm()		
+		args = {'form': form}
+
+		form_info = TemplateForm(request.POST)
+		if form_info.is_valid():
+			print('post accepted.')
+			msg = request.POST.get('message', '')
+			team_name = request.POST.get('team_name', '')
+			size= request.POST.get('size', 'A3')
+			#print the message out too
+			context = {
+				"message": msg,
+				"team_name": team_name,
+				}
+
+			pdf = render_to_pdf(request, 'pdf/poster.html', context, size=size)
+			if pdf:
+				
+				response =  HttpResponse(pdf, content_type='application/pdf')
+				filename = "diversityFIRST"
+				content = "inline; filename='%s'" %(filename)
+				download = request.GET.get("download")
+				if download:
+					content = "attachment; filename='%s'" %(filename)
+				response['Content-Disposition'] = content
+				return response
+			return HttpResponse("Not Found")
+
+		else:
+			print(False)
+			return render(request, self.template_name, args)
 
 class PosterView(PDFTemplateView):
 	template_name = 'hello.html'
