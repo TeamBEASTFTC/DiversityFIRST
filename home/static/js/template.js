829d8c8a7fcdd321953700587169740365c7541c
fhall21 <felix.p.hall@gmail.com>
2018-09-28 16:45:55 +1000

    there are errors in the template.js code. All that is currently JS code may be broken, but is not being used as of now. To do: proper resizing for the preview text, a small message easily overflows.

--- home/static/js/template.js
+++ home/static/js/template.js
@@ -8,7 +8,7 @@ $(function(){
 });
 
 
-
+/*
 var row_c = document.getElementById("GridBox");
 var sm_down = document.getElementById("sm_down");
 var lg_down = document.getElementById("lg_down");
@@ -21,18 +21,22 @@ if (row_c.offsetWidth >= 720){
 	lg_down.style.display = 'block';
 	sm_down.style.display = 'none';
 }
-
-/* Make preview set smaller once broken*/
+*/
+//Make preview set smaller once broken
+/* 
 var prebox1 = document.getElementById("box_js");
 var width1 = prebox.offsetWidth;
 
 var prebox2 = document.getElementById("box_js2");
 var width2 = prebox2.offsetWidth;
 prebox2.style.padding-top = width2 * 0.7071
+*/
 
-/*if (prebox.getComputedStyle.width >= "500px"){ */
+/*
+//if (prebox.getComputedStyle.width >= "500px"){ 
 if ((width1 >= 500) || (width2 >= 500)){
 	prebox1.style.height = (500*0.7071);
 	alert("done");
 	prebox2.style.height = (500*0.7071);
 }
+*/
