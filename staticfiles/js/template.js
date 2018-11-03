ffbf63e11dc24e0fdc266954025762438946dfa2
Felix <felix.p.hall@gmail.com>
2018-11-04 08:45:21 +1000

    called collectstatic and tried doing heroku migrations

---
+++ staticfiles/js/template.js
@@ -0,0 +1,78 @@
+function prevSizing(){
+	var width = $('#box_js2').width();
+	var h = width * (210 / 297);
+	//dimensioning it to be roughly A4 size
+	$('#box_js2').height(h);
+	$('#box_js').height(h);
+
+	var size = 0.05 * width
+	var padR = width * 0.275
+	
+	// font-size
+	$('.pretitle').css('font-size', size);
+	$('.predetails').css('font-size', size);
+
+	//message padding (right)
+	$('.predetails').css('padding-right', padR);
+
+	//message so I know
+}
+
+function downBtnLoc(){
+	if ($(window).width() < 700){
+		//grid off
+		$('#sm_down').css('display', 'none');
+		$('#lg_down').css('display', 'block');
+	} else {
+		//grid on
+		$('#sm_down').css('display', 'block');
+		$('#lg_down').css('display', 'none');
+	}
+}
+
+
+$(function(){
+	$('#message_field').change(function(){
+		$('#message').html($('#message_field').val());
+	});
+	prevSizing();
+	downBtnLoc();
+});
+
+$(window).resize(function(){
+	prevSizing();
+	downBtnLoc();
+});
+
+/*
+var row_c = document.getElementById("GridBox");
+var sm_down = document.getElementById("sm_down");
+var lg_down = document.getElementById("lg_down");
+
+
+if (row_c.offsetWidth >= 720){
+	lg_down.style.display = 'none';
+	sm_down.style.display = 'block';
+} else {
+	lg_down.style.display = 'block';
+	sm_down.style.display = 'none';
+}
+*/
+//Make preview set smaller once broken
+/* 
+var prebox1 = document.getElementById("box_js");
+var width1 = prebox.offsetWidth;
+
+var prebox2 = document.getElementById("box_js2");
+var width2 = prebox2.offsetWidth;
+prebox2.style.padding-top = width2 * 0.7071
+*/
+
+/*
+//if (prebox.getComputedStyle.width >= "500px"){ 
+if ((width1 >= 500) || (width2 >= 500)){
+	prebox1.style.height = (500*0.7071);
+	alert("done");
+	prebox2.style.height = (500*0.7071);
+}
+*/
