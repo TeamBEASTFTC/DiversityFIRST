1458c194e3a5c7125dfbaa2b9dfd2f00528d78bc
Felix <felix.p.hall@gmail.com>
2019-11-21 07:06:05 +1000

    registered the ambassador model to the admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from home.models import TemplateData, TeamPhoto
+from home.models import TemplateData, TeamPhoto, AmbassadorData
 # Register your models here.
 admin.site.register(TemplateData)
+
+class AmbassadorAdmin(admin.ModelAdmin):
+	list_display = (
+		'team_name','team_email',)
+	def get_queryset(self, request):
+		return AmbassadorData.objects.all()
+admin.site.register(AmbassadorData, AmbassadorAdmin)
 
 class TeamPhotoAdmin(admin.ModelAdmin):
 	list_display = (
