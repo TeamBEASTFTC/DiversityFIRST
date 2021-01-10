600430539b270228e3aeb48236c872df024361d6
Felix <felix.p.hall@gmail.com>
2021-01-10 11:03:02 +1000

    updated s3 bucket name

--- diversityFIRST/settings.py
+++ diversityFIRST/settings.py
@@ -171,7 +171,7 @@ STATICFILES_DIRS = [
 ]
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'beast-diversity-first-asset'
+AWS_STORAGE_BUCKET_NAME = 'beast-diversity-first'
 
 AWS_DEFAULT_ACL = 'public-read'
 
